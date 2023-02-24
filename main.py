bf2a03a0f9b1e35c026dcb727761e71472b6797e
Lindsay Gatawa <123521323+lindsaygatawa@users.noreply.github.com>
2023-02-24 13:59:07 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+adjective = input("Enter Adjective: ")
+verb = input("Enter a verb: ")
+verb2 = input("Enter a verb: ")
+celebrity = input("Enter a celebrity name: ")
+verb3 = input("Enter another verb: ")
+Story = f"Computer Programming is {adjective} and I love it because I love to {verb} all the time. When Im not coding I like to play {verb2} \
+        like {celebrity} and after I drink water to stay{verb3}"
+story = f"Yestrday I was {verb} sports and I had an {adjective} time, After that me and my family went {verb2}. My sister decided to {verb3} \
+                a movie which featured {celebrity}"
+print(Story)
+print(story)
